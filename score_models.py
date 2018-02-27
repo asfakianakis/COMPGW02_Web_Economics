7df2c1f3ebf53d55c4febde4f8201c5844373ee4
dwane <dwanev@gmail.com>
2018-02-27 10:02:31 +0000

    routine to score a model - now returns 2 dataframs - a summary, and a full df

--- score_models.py
+++ score_models.py
@@ -2,14 +2,11 @@
 import logging
 import os
 import numpy as np
+import pandas as pd
 
 from data_loader import DataLoader
 
-
-
-
 class Scorer(object):
-
 
     def second_largest(self,numbers):
         count = 0
@@ -25,39 +22,52 @@ class Scorer(object):
 
     def set_df(self,df,column_names, budget, reserve):
 
+        df = df.copy(True)
         balances = np.ones(len(column_names)) * budget
-
-        df_t = df[column_names]
+        df_teams_bid_prices_only = df[column_names]
         df['winner'] = -1
         df['price'] = 0
         logging.info('calculating price paid (second highest). This takes some time...')
         for i in range(len(df)):
-            prices = df_t.loc[i].values
+            prices = df_teams_bid_prices_only.loc[i].values
             valid1 = balances > prices
             valid2 = prices > 0.0
             valid3 = valid1 * valid2
             new_prices = valid3 * prices
             winner = np.argmax(new_prices)
-            price = self.second_largest(new_prices)
-            price = np.max([reserve,price])
-            df.loc[i, 'price'] = price
-            df.loc[i, 'winner'] = winner
-            balances[winner] -= price
+            if (new_prices[winner]>0):
+                price = self.second_largest(new_prices)
+                price = np.max([reserve,price])
+                df.loc[i, 'price'] = price
+                df.loc[i, 'winner'] = winner
+                balances[winner] -= price
 
-            if i % 50 == 0:
-                logging.info('%f complete',(float(i)/len(df)))
+            if i % int(len(df)/49) == 0:
+                logging.info('%.2f complete',(float(i)/len(df)))
+
+            if np.sum(balances > 0.0) == 0: # if no balances are positive, we don't need to process the rest of the data frame
+                break
 
         logging.info('calculating wins by team')
         wins_by_team = df.winner.value_counts()
-
         teams = df.winner.unique()
-        for team in teams:
 
-            t = df.loc[df['winner'] == team]
-            clicks = np.sum(t['click'].values)
-            logging.info('team ' + str(team) + ' wins:' + str(wins_by_team[team]) + ' clicks:' + str(clicks))
+        team_names = []
+        team_wins = []
+        team_clicks = []
+        for team_index in teams:
+            if team_index >= 0:
+                temp_df = df.loc[df['winner'] == team_index]
+                clicks = np.sum(temp_df['click'].values)
+                team_name = df_teams_bid_prices_only.columns[team_index]
+                team_names.append(team_name)
+                team_wins.append(wins_by_team[team_index])
+                team_clicks.append(clicks)
 
-        print(df.head())
+                logging.info(
+                    'team ' + team_name + ' ' + str(team_index) + ' wins:' + str(wins_by_team[team_index]) + ' clicks:' + str(clicks))
+        df_summary = pd.DataFrame({"team_name": team_names, "win": team_wins, "click": team_clicks})
+        return(df_summary,df)
 
 
 if __name__ == '__main__':
@@ -71,28 +81,24 @@ if __name__ == '__main__':
     logging.info('starting train_via_rf.py')
 
     target_col_name = 'click'
-    train_filename = 'validation.csv'
+    data_filename = 'validation.csv'
 
-    #train_filename = 'train.cutdown.csv'
-
-    logging.info('loading DF '+train_filename)
+    logging.info('loading DF ' + data_filename)
     train_dl = DataLoader()
-    train_dl.load_file(path, train_filename)
+    train_dl.load_file(path, data_filename)
     df = train_dl.get_df_copy()
 
-    #print(df.head())
     df_t = df[['click']].copy(True)
-
     df_t['bidTeam1'] = np.random.randint(227, 230, df_t.shape[0])
     df_t['bidTeam2'] = np.ones(df_t.shape[0]) * 278
     df_t['bidTeam3'] = np.random.randint(297, 301, df_t.shape[0])
 
-    # df_t['bidTeam3'] = 3
-    # df_t['bidTeam2'] = 2
-    # df_t['bidTeam1'] = 1
+    print(df_t.head())
 
     s = Scorer()
-    s.set_df(df_t,['bidTeam1','bidTeam2','bidTeam3'],6250000, 227)
+    df_summary, df_full = s.set_df(df_t,['bidTeam1','bidTeam2','bidTeam3'],270000000, 227)
+
+    print(df_summary.head())
 
 
 
